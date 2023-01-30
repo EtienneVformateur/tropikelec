f8c726963c27464d779e3dd407c4bbaa20b9b9b1
Istinnie <n.istinnie.velonkasina@gmail.com>
2023-01-30 13:55:15 +0400

    Page Téléchargements - Sauvegarde de fichiers - Paramètres

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'ligneBatterie.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +14,156 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TropikElec',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Liste des équipements'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({super.key, required this.title});
 
   final String title;
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  // final List<BluetoothDevice> devicesList = new List<BluetoothDevice>();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // _addDeviceTolist(final BluetoothDevice device) {
+  //   if (!widget.devicesList.contains(device)) {
+  //     setState(() {
+  //       widget.devicesList.add(device);
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        centerTitle: true,
+        // leading: Icon(
+        //   Icons.menu,
+        //   size: 34,
+        // ),
+        title: Text(
+          widget.title,
+          textAlign: TextAlign.center,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 10, 0),
+            child: InkWell(
+              child: const Icon(Icons.refresh),
+              onTap: () {
+                //action code when clicked
+                demandePermission();
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Menu',
+                  style: TextStyle(fontSize: 25, color: Colors.white)),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.menu,
+              ),
+              title: const Text('Liste des appareils'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.play_circle_filled,
+              ),
+              title: const Text('Demo'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.save_alt,
+              ),
+              title: const Text('Sauvegarde de fichiers'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.file_download,
+              ),
+              title: const Text('Téléchargements'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.question_mark,
+              ),
+              title: const Text('Paramètres'),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(
+        child: Column(
+          children: const [
+            LigneBatterie(
+              nom: "Smart Lithium",
+              amperage: 150,
+              identifiant: "1225341234324",
+              signal: 3,
+              isSwitched: true,
+            ),
+            LigneBatterie(
+              nom: "Smart Lithium",
+              amperage: 100,
+              identifiant: "1235346323425",
+              signal: 2,
+              isSwitched: false,
+            ),
+            LigneBatterie(
+              nom: "Smart Lithium",
+              amperage: 100,
+              identifiant: "13245354545342",
+              signal: 1,
+              isSwitched: true,
+            )
+          ],
+        ),
+      ),
     );
+  }
+}
+
+void demandePermission() async {
+  var permissionScan = await Permission.bluetoothConnect.request();
+  var permissionConnect = await Permission.bluetoothScan.request();
+  if (permissionScan.isGranted && permissionConnect.isGranted) {
+    print("Permission ");
   }
 }
